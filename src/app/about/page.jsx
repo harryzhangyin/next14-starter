90b411201e72566666f41b2624485b1f80394253
harryzhangyin <149778720+harryzhangyin@users.noreply.github.com>
2024-03-22 16:44:53 +0000

    2024.03.23

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,15 @@
+import Image from "next/image"
+import styles from "./about.module.css"
+
 const AboutPage = () => {
-    return <div>AboutPage</div>;
-  };
+    return (
+      <div>
+        <div className={styles.imgContainer}>
+          <Image src="https://images.pexels.com/photos/19824001/pexels-photo-19824001/free-photo-of-a-city-street-with-a-church-and-mountains-in-the-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill/>
+        </div>
+        
+      </div>
+  )
+};
   
   export default AboutPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from './home.module.css';
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+  <div className={styles.container}>
+    <div className={styles.textContainer}>
+      <h1>Creative Thoughts Agency.</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+        blanditiis adipisci minima reiciendis a autem assumenda dolore.
+      </p>
+      <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+      </div>
+      <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
+      </div>
+    </div>
+  </div>
+  );
 };
 
 export default Home;
