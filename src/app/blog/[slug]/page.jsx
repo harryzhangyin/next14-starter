ce5c62c81af3e0cd49ef2b25bf6916f604197eb5
harryzhangyin <149778720+harryzhangyin@users.noreply.github.com>
2024-03-24 10:18:42 +0000

    4858

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./singlePost.module.css";
 import PostUser from "@/components/postUser/postUser";
 import { Suspense } from "react";
+import { getPost } from "@/lib/data";
 
 // const getData = async (slug)=>{
 //   const res = await fetch (`https://jsonplaceholder.typicode.com/posts/${slug}`)
@@ -18,26 +19,29 @@ const SinglePostPage = async ({params}) => {
 
     const {slug} = params;
 
-    const post = await getData(slug);
+    // const post = await getData(slug);
+    const post = await getPost(slug);
     return (
       <div  className={styles.container}>
         <div className={styles.imgContainer}>
           <Image src="https://images.pexels.com/photos/19824001/pexels-photo-19824001/free-photo-of-a-city-street-with-a-church-and-mountains-in-the-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img}/>
         </div>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>{post.title}</h1>
+          <h1 className={styles.title}>{post?.title}</h1>
           <div className={styles.detail}>
             <Image className={styles.avatar} src="https://images.pexels.com/photos/19824001/pexels-photo-19824001/free-photo-of-a-city-street-with-a-church-and-mountains-in-the-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={50} height={50}/>
-              <Suspense fallback={<div>Loading...</div>}>
-                <PostUser userId = {post.userId}/>
-              </Suspense>
+              {post && (
+                <Suspense fallback={<div>Loading...</div>}>
+                  <PostUser userId = {post.userId}/>
+                </Suspense>
+              )}
             <div className={styles.detailText}>
               <span className={styles.detailTitle}>Published</span>
               <span className={styles.detailValue}>01.01.2024</span>
             </div>
           </div>
           <div className={styles.content}>
-           { post.body}
+           {post.body}
           </div>
         </div>
       </div>
